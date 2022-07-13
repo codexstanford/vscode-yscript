903548e7e5bb2d632525c2b3c94302200d5986e6
William O'Hanley <me@wohanley.com>
2022-07-13 13:32:43 -0700

    CustomEditor  for graph

--- src/extension.ts
+++ src/extension.ts
@@ -3,28 +3,31 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 
 const graphHtmlRelativePath = path.join('resources', 'graph', 'index.html');
-const graphJsRelativePath   = path.join('resources', 'graph', 'js', 'compiled', 'app.js');
+const graphJsRelativePath = path.join('resources', 'graph', 'js', 'compiled', 'app.js');
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('yscript.graph.show', () => showGraph(context)));
+	context.subscriptions.push(YscriptGraphEditorProvider.register(context));
 }
 
-function showGraph(context: vscode.ExtensionContext) {
-	// Load HTML source from resources
-	const html = fs.readFile(
-		path.join(context.extensionPath, graphHtmlRelativePath), 
-		{ encoding: 'utf-8' });
+class YscriptGraphEditorProvider implements vscode.CustomTextEditorProvider {
+	constructor(private readonly context: vscode.ExtensionContext) { }
 
-	// Show in webview panel
-	html.then(htmlString => {
-		const panel = vscode.window.createWebviewPanel(
-			'yscriptGraph',
-			"yscript Graph",
-			vscode.ViewColumn.Two,
-			{ enableScripts: true });
+	public static register(context: vscode.ExtensionContext): vscode.Disposable {
+		return vscode.window.registerCustomEditorProvider('yscript.graph', new YscriptGraphEditorProvider(context));
+	}
 
-		panel.webview.html = _assembleGraphHtml(htmlString, panel.webview, context);
-	});
+	public async resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): Promise<void> {
+		// Load HTML source from resources
+		const html = fs.readFile(
+			path.join(this.context.extensionPath, graphHtmlRelativePath),
+			{ encoding: 'utf-8' });
+
+		// Show in webview panel
+		return html.then(htmlString => {
+			webviewPanel.webview.options = { enableScripts: true };
+			webviewPanel.webview.html = _assembleGraphHtml(htmlString, webviewPanel.webview, this.context);
+		});
+	}
 }
 
 /**
@@ -34,9 +37,9 @@ function showGraph(context: vscode.ExtensionContext) {
  * 
  * @param htmlString the raw HTML string as read from disk
  * @param webview the webview into which the HTML will be rendered
- * @returns {String} the HTML with appropriate replacements made
+ * @returns {string} the HTML with appropriate replacements made
  */
-function _assembleGraphHtml(htmlString: String, webview: vscode.Webview, context: vscode.ExtensionContext) {
+function _assembleGraphHtml(htmlString: String, webview: vscode.Webview, context: vscode.ExtensionContext): string {
 	const jsUri = vscode.Uri.file(path.join(context.extensionPath, graphJsRelativePath));
 	return htmlString.replace("/js/compiled/app.js", webview.asWebviewUri(jsUri).toString());
 }
