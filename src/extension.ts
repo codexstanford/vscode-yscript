c540fc255600e11a42328c285fd3b034e061e5db
William O'Hanley <me@wohanley.com>
2022-07-13 14:04:31 -0700

    Read yscript code from document in vscode

--- src/extension.ts
+++ src/extension.ts
@@ -6,26 +6,32 @@ const graphHtmlRelativePath = path.join('resources', 'graph', 'index.html');
 const graphJsRelativePath = path.join('resources', 'graph', 'js', 'compiled', 'app.js');
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(YscriptGraphEditorProvider.register(context));
+	context.subscriptions.push(
+		vscode.window.registerCustomEditorProvider('yscript.graph', new YscriptGraphEditorProvider(context)));
 }
 
 class YscriptGraphEditorProvider implements vscode.CustomTextEditorProvider {
 	constructor(private readonly context: vscode.ExtensionContext) { }
 
-	public static register(context: vscode.ExtensionContext): vscode.Disposable {
-		return vscode.window.registerCustomEditorProvider('yscript.graph', new YscriptGraphEditorProvider(context));
-	}
-
-	public async resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): Promise<void> {
-		// Load HTML source from resources
-		const html = fs.readFile(
+	public resolveCustomTextEditor(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void {
+		// Start loading HTML source from resources
+		const rawHtml = fs.readFile(
 			path.join(this.context.extensionPath, graphHtmlRelativePath),
 			{ encoding: 'utf-8' });
 
-		// Show in webview panel
-		return html.then(htmlString => {
+		const textChangeSub = vscode.workspace.onDidChangeTextDocument(evt => {
+			if (evt.document.uri.toString() === document.uri.toString()) {
+				_updateGraphFromCode(webviewPanel.webview, document.getText());
+			}
+		});
+
+		webviewPanel.onDidDispose(textChangeSub.dispose);
+
+		// Render HTML into webview
+		rawHtml.then(raw => {
 			webviewPanel.webview.options = { enableScripts: true };
-			webviewPanel.webview.html = _assembleGraphHtml(htmlString, webviewPanel.webview, this.context);
+			webviewPanel.webview.html = _assembleGraphHtml(raw, webviewPanel.webview, this.context);
+			_updateGraphFromCode(webviewPanel.webview, document.getText());
 		});
 	}
 }
@@ -42,4 +48,11 @@ class YscriptGraphEditorProvider implements vscode.CustomTextEditorProvider {
 function _assembleGraphHtml(htmlString: String, webview: vscode.Webview, context: vscode.ExtensionContext): string {
 	const jsUri = vscode.Uri.file(path.join(context.extensionPath, graphJsRelativePath));
 	return htmlString.replace("/js/compiled/app.js", webview.asWebviewUri(jsUri).toString());
+}
+
+function _updateGraphFromCode(webview: vscode.Webview, text: string) {
+	webview.postMessage({
+		'type': 'yscript.graph.codeUpdated',
+		'text': text
+	});
 }
